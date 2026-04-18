bf5e7ac2eeeb594976260705ab3cedd038abe947
Sivasiby <sibiraj456@gmail.com>
2026-04-18 22:09:44 +0530

    fix: nativewind v4 setup for bare React Native

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./App.{js,jsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   presets: [require("nativewind/preset")],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
